9179743fc3390d130e5705205264ce672832cc13
Rafael J. Martínez García <neofarin@gmail.com>
2022-07-31 14:07:56 +0200

    creo lista de equipos

---
+++ all-teams.js
@@ -0,0 +1,17 @@
+export default [
+    "Inglaterra",
+    "Austria",
+    "Noruega",
+    "Irlanda del Norte",
+    "Alemania",
+    "Dinamarca",
+    "España",
+    "Finlandia",
+    "Países Bajos",
+    "Suecia",
+    "Portugal",
+    "Suiza",
+    "Francia",
+    "Italia",
+    "Bélgica",
+    "Islandia",]
