eea6a22db605506ae03661b04518156f383a275f
Rafael J. Martínez García <neofarin@gmail.com>
2022-07-31 14:26:46 +0200

    creo archivo index.js, carpeta classes y archivos euro y fase de grupos

---
+++ index.js
@@ -0,0 +1,7 @@
+import GroupStage from "./classes/GroupStage.js"
+import allTeams from "./all-teams.js";
+
+
+const groupStage = new GroupStage(allTeams);
+
+const playoffs = new Playoffs(allTeams);
