bc3d1efcdf1ae4d28a793063ce9f8a94e47b9d0e
Rafael J. Martínez García <neofarin@gmail.com>
2022-07-31 20:54:44 +0200

    agrego arhivos en classes, el package.json y edito el index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import GroupStage from "./classes/GroupStage.js"
 import allTeams from "./all-teams.js";
+import Playoffs from "./classes/Playoffs.js";
+
+const groupStage = new GroupStage(allTeams);
+
+const playoffs = new Playoffs(allTeams);
 
 
